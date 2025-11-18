b11313f39f952ff449a7c3f2f76f1c43c504bf31
No0B@ckSappi3 <79768241+Kvngtheta@users.noreply.github.com>
2025-11-17 18:45:23 -0600

    Refactor myfunctions.h to use std namespace

--- myfunctions.h
+++ myfunctions.h
@@ -4,11 +4,9 @@
 #include <thread>
 #include <chrono>
 
-using namespace std;
+std::string buildUniqueID();
 
-string buildUniqueID();
+bool sendPostRequest(const std::string& host, int port, const std::string& path, 
+                     const std::string& customData, const std::string& uniqueID, char validate);
 
-bool sendPostRequest(const string& host, int port, const string& path, 
-                     const string& customData, const string& uniqueID, char validate);
-
-void agentbeaconFunction(const string& ip, int port);
+void agentbeaconFunction(const std::string& ip, int port);
